158332f00b59f8fa99db1dec2c6d66abea2bf954
Kuh <73922679+ramadhankukuh@users.noreply.github.com>
2022-05-22 23:26:07 +0700

    Add files via upload

--- kuh-cek-nick/trueID.php
+++ kuh-cek-nick/trueID.php
@@ -1,1 +1,36 @@
-
+<?php 
+if(isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "http://api-xyz.com/trueid/freefire/?id=".$id."&token=NguyenThuWan");
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    $res = json_decode($result,true);
+    if(isset($res['nickname']))
+    {
+    $print = array(
+                'result' => array(
+                    'status' => '200',
+                    'Author' => 'Nguyen Thu Wan'
+                ),
+                'nickname' => $res['nickname'],
+                'userid' => $res['userid']
+            );
+        $hasil = json_encode($print, JSON_PRETTY_PRINT);
+    }else{
+        $print = array(
+                'result' => array(
+                    'status' => '400',
+                    'Author' => 'Nguyen Thu Wan'
+                ),
+                'error_msg' => 'Invalid id'
+            );
+        $hasil = json_encode($print, JSON_PRETTY_PRINT);
+    }
+    print_r($hasil);
+    }
+?>
